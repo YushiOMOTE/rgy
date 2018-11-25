ef10094697972a52ca66d9a9f72edd7c308214c1
Yushi OMOTE <yushiomote@gmail.com>
2018-11-25 16:02:53 +0900

    Add unittest for alu

--- core/src/alu.rs
+++ core/src/alu.rs
@@ -56,3 +56,49 @@ pub fn add16e(p: u16, q: u8, c: bool) -> (u16, bool, bool, bool) {
     let (v, h, c, z) = add(16, p as usize, signed(q) as usize, c, 4, 8);
     (v as u16, h, c, z)
 }
+
+#[test]
+fn test_add8() {
+    assert_eq!(add8(0x12, 0x22, false), (0x34, false, false, false));
+    assert_eq!(add8(0x12, 0x22, true), (0x35, false, false, false));
+    assert_eq!(add8(0x12, 0x2f, false), (0x41, true, false, false));
+    assert_eq!(add8(0x12, 0x2f, true), (0x42, true, false, false));
+    assert_eq!(add8(0x12, 0xf0, false), (0x02, false, true, false));
+    assert_eq!(add8(0x12, 0xf0, true), (0x03, false, true, false));
+    assert_eq!(add8(0x0a, 0xfa, false), (0x04, true, true, false));
+    assert_eq!(add8(0x0a, 0xfa, true), (0x05, true, true, false));
+    assert_eq!(add8(0x00, 0x00, false), (0x00, false, false, true));
+    assert_eq!(add8(0x20, 0xe0, false), (0x00, false, true, true));
+    assert_eq!(add8(0x08, 0xf8, false), (0x00, true, true, true));
+    assert_eq!(add8(0x07, 0xf8, true), (0x00, true, true, true));
+}
+
+#[test]
+fn test_sub8() {
+    assert_eq!(sub8(0x12, 0x10, false), (0x02, false, false, false));
+    assert_eq!(sub8(0x34, 0x22, true), (0x11, false, false, false));
+    assert_eq!(sub8(0x32, 0x2f, false), (0x03, true, false, false));
+    assert_eq!(sub8(0x32, 0x2e, true), (0x03, true, false, false));
+    assert_eq!(sub8(0x12, 0xf0, false), (0x22, false, true, false));
+    assert_eq!(sub8(0x12, 0xe0, true), (0x31, false, true, false));
+    assert_eq!(sub8(0x0a, 0xef, false), (0x1b, true, true, false));
+    assert_eq!(sub8(0x20, 0x5a, true), (0xc5, true, true, false));
+    assert_eq!(sub8(0x12, 0x12, false), (0x00, false, false, true));
+    assert_eq!(sub8(0x88, 0x87, true), (0x00, false, false, true));
+}
+
+#[test]
+fn test_add16() {
+    assert_eq!(add16(0x1200, 0x1000, false), (0x2200, false, false, false));
+    assert_eq!(add16(0x1134, 0x1222, true), (0x2357, false, false, false));
+    assert_eq!(add16(0xf231, 0x2a13, false), (0x1c44, false, true, false));
+    assert_eq!(add16(0xf231, 0x2a13, true), (0x1c45, false, true, false));
+    assert_eq!(add16(0xf631, 0x2a03, false), (0x2034, true, true, false));
+    assert_eq!(add16(0xf631, 0x2a03, true), (0x2035, true, true, false));
+}
+
+#[test]
+fn test_signed() {
+    assert_eq!(signed(0x0a), 0x000a);
+    assert_eq!(signed(0x8a), 0xff8a);
+}
