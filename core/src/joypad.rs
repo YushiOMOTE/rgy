8455b56401ac8309ce342d9573732963ecd633d2
Yushi Omote <yushiomote@gmail.com>
2019-06-11 17:39:21 +0900

    Fix joypad selector logic to avoid SGB misdetection
    If neither buttons nor direction are selected, just return 0x0f on read from joypad register.
    Otherwise, Donkey Kong Land goes into SGB mode and uses wrong color palette.

--- core/src/joypad.rs
+++ core/src/joypad.rs
@@ -27,19 +27,22 @@ impl IoHandler for Joypad {
 
             debug!("Joypad read: dir: {:02x}", self.select);
 
-            let mut value = 0xff & self.select;
+            let mut value = 0;
+
             if self.select & 0x10 == 0 {
                 value |= if p(Key::Right) { 0x00 } else { 0x01 };
                 value |= if p(Key::Left) { 0x00 } else { 0x02 };
                 value |= if p(Key::Up) { 0x00 } else { 0x04 };
                 value |= if p(Key::Down) { 0x00 } else { 0x08 };
-            }
-            if self.select & 0x20 == 0 {
+            } else if self.select & 0x20 == 0 {
                 value |= if p(Key::A) { 0x00 } else { 0x01 };
                 value |= if p(Key::B) { 0x00 } else { 0x02 };
                 value |= if p(Key::Select) { 0x00 } else { 0x04 };
                 value |= if p(Key::Start) { 0x0 } else { 0x08 };
+            } else {
+                value = 0x0f;
             }
+
             MemRead::Replace(value)
         } else {
             MemRead::PassThrough
@@ -48,7 +51,7 @@ impl IoHandler for Joypad {
 
     fn on_write(&mut self, _mmu: &Mmu, addr: u16, value: u8) -> MemWrite {
         if addr == 0xff00 {
-            self.select = value;
+            self.select = value & 0xf0;
         }
         MemWrite::PassThrough
     }
