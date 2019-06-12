YushiOMOTE/rgy

commit ef0e9c14d65b4da898a7ae956d044e2d5a71037a

Print warning instead of panic on accessing unsupported GPU registers
Because some are accessed while running ROM tests

core/src/gpu.rs
@@ -532,7 +532,8 @@ impl IoHandler for Gpu {
         } else if addr == 0xff68 || addr == 0xff69 || addr == 0xff6a || addr == 0xff6b {
             unimplemented!("read color")
         } else {
-            unreachable!("Unsupported gpu register read: {:04x}", addr)
+            warn!("Unsupported GPU register read: {:04x}", addr);
+            MemRead::Replace(0)
         }
     }
 
@@ -575,6 +576,10 @@ impl IoHandler for Gpu {
             unimplemented!("write color")
         }
 
-        MemWrite::PassThrough
+        warn!(
+            "Unsupported GPU register is written: {:04x} {:02x}",
+            addr, value
+        );
+        MemWrite::Block
     }
 }

commit 793cd84978b4d4bcbc87da91a31f5e48005a3498

Fix regression for printing warning on GPU registers
Caused by ef0e9c14d65b4da898a7ae956d044e2d5a71037a

core/src/gpu.rs
@@ -574,12 +574,13 @@ impl IoHandler for Gpu {
             unimplemented!("write ff4f (vram bank)")
         } else if addr == 0xff68 || addr == 0xff69 || addr == 0xff6a || addr == 0xff6b {
             unimplemented!("write color")
+        } else {
+            warn!(
+                "Unsupported GPU register is written: {:04x} {:02x}",
+                addr, value
+            );
         }
 
-        warn!(
-            "Unsupported GPU register is written: {:04x} {:02x}",
-            addr, value
-        );
-        MemWrite::Block
+        MemWrite::PassThrough
     }
 }