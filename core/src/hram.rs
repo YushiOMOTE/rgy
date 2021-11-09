ad9b4f03e8192a2cfd343bb671e81c773f7f1ec3
YushiOMOTE <yushiomote@gmail.com>
2021-11-10 08:44:06 +0900

    Add test for hram

--- core/src/hram.rs
+++ core/src/hram.rs
@@ -16,3 +16,47 @@ impl Hram {
         self.bank[addr as usize - 0xff80] = v;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_set() {
+        let mut m = Hram::new();
+        for i in 0..0x7f {
+            m.set8(0xff80 + i, i as u8);
+        }
+        for i in 0..0x7f {
+            assert_eq!(m.get8(0xff80 + i), i as u8);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_too_low() {
+        let mut m = Hram::new();
+        m.set8(0xff7f, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_too_high() {
+        let mut m = Hram::new();
+        m.set8(0xffff, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_too_low() {
+        let m = Hram::new();
+        m.get8(0xff7f);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_too_high() {
+        let m = Hram::new();
+        m.get8(0xffff);
+    }
+}
