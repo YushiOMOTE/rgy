2e13fb919bcdda9f7710eaa1ab7f95dfca79f346
YushiOMOTE <yushiomote@gmail.com>
2021-11-10 08:40:06 +0900

    Add test for work ram

--- core/src/wram.rs
+++ core/src/wram.rs
@@ -41,3 +41,83 @@ impl Wram {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_default_bank() {
+        let m = Wram::new();
+        assert_eq!(m.get_bank(), 1);
+    }
+
+    #[test]
+    fn test_select_bank() {
+        let mut m = Wram::new();
+
+        // fill bank 0
+        for i in 0..0x1000 {
+            m.set8(0xc000 + i, 0xf)
+        }
+
+        // fill bank 1-7
+        for b in 1..8 {
+            m.select_bank(b);
+            for i in 0..0x1000 {
+                m.set8(0xd000 + i, b)
+            }
+        }
+
+        // check each bank
+        for b in 1..8 {
+            m.select_bank(b);
+            for i in 0..0x1000 {
+                assert_eq!(m.get8(0xc000 + i), 0xf);
+                assert_eq!(m.get8(0xd000 + i), b);
+            }
+        }
+    }
+
+    #[test]
+    fn test_mirror() {
+        let mut m = Wram::new();
+
+        for v in 1..4 {
+            for i in 0..0x1dff {
+                m.set8(0xc000 + i, v);
+            }
+            for i in 0..0x1dff {
+                assert_eq!(m.get8(0xe000 + i), v);
+            }
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_too_low() {
+        let mut m = Wram::new();
+        m.set8(0xbfff, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_too_high() {
+        let mut m = Wram::new();
+        m.set8(0xfe00, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_too_low() {
+        let m = Wram::new();
+        m.get8(0xbfff);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_too_high() {
+        let m = Wram::new();
+        m.get8(0xfe00);
+    }
+}
